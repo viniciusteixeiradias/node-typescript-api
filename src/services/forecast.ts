62dfc72f4be0dffdd8d91311b917d491e99576e7
viniciusteixeiradias <viniciustdias2001@gmail.com>
2023-01-30 00:16:55 -0300

    create mapForecastByTime method

--- src/services/forecast.ts
+++ src/services/forecast.ts
@@ -1,4 +1,4 @@
-import { ForecastPoint, StormGlass } from '@src/clients/stormGlass';
+import { StormGlass, ForecastPoint } from '@src/clients/stormGlass';
 
 export enum BeachPosition {
   S = 'S',
@@ -17,30 +17,49 @@ export interface Beach {
 
 export interface BeachForecast extends Omit<Beach, 'user'>, ForecastPoint {}
 
+export interface TimeForecast {
+  time: string;
+  forecast: BeachForecast[];
+}
+
 export class Forecast {
   constructor(protected stormGlass = new StormGlass()) {}
 
   public async processForecastForBeaches(
     beaches: Beach[]
-  ): Promise<BeachForecast[]> {
+  ): Promise<TimeForecast[]> {
     const pointsWithCorrectSources: BeachForecast[] = [];
-
     for (const beach of beaches) {
       const points = await this.stormGlass.fetchPoints(beach.lat, beach.lng);
       const enrichedBeachData = points.map((e) => ({
+        ...{},
         ...{
           lat: beach.lat,
           lng: beach.lng,
           name: beach.name,
           position: beach.position,
-          rating: 1,
+          rating: 1, //need to be implemented
         },
         ...e,
       }));
-
       pointsWithCorrectSources.push(...enrichedBeachData);
     }
+    return this.mapForecastByTime(pointsWithCorrectSources);
+  }
 
-    return pointsWithCorrectSources;
+  private mapForecastByTime(forecast: BeachForecast[]): TimeForecast[] {
+    const forecastByTime: TimeForecast[] = [];
+    for (const point of forecast) {
+      const timePoint = forecastByTime.find((f) => f.time === point.time);
+      if (timePoint) {
+        timePoint.forecast.push(point);
+      } else {
+        forecastByTime.push({
+          time: point.time,
+          forecast: [point],
+        });
+      }
+    }
+    return forecastByTime;
   }
 }
