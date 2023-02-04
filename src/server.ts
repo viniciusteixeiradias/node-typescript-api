67bd023f2ff6424a74aefabb5bb9fa568d716af7
viniciusteixeiradias <viniciustdias2001@gmail.com>
2023-02-03 22:57:37 -0300

    add mongoose

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,32 @@
 import './util/module-alias';
 import { Server } from '@overnightjs/core';
+import { Application } from 'express';
 import bodyParser from 'body-parser';
 import { ForecastController } from './controllers/forecast';
-import { Application } from 'express';
+import * as database from '@src/database';
 
 export class SetupServer extends Server {
+  /*
+   * same as this.port = port, declaring as private here will
+   * add the port variable to the SetupServer instance
+   */
   constructor(private port = 3000) {
     super();
   }
 
-  public init(): void {
+  /*
+   * We use a different method to init instead of using the constructor
+   * this way we allow the server to be used in tests and normal initialization
+   */
+  public async init(): Promise<void> {
     this.setupExpress();
     this.setupControllers();
+    await this.databaseSetup();
   }
 
   private setupExpress(): void {
     this.app.use(bodyParser.json());
+    this.setupControllers();
   }
 
   private setupControllers(): void {
@@ -25,5 +36,13 @@ export class SetupServer extends Server {
 
   public getApp(): Application {
     return this.app;
+  }
+
+  private async databaseSetup(): Promise<void> {
+    await database.connect();
+  }
+
+  public async close(): Promise<void> {
+    await database.close();
   }
 }
